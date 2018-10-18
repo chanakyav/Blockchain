5f9ddb6b2d3aafc438c15cf443b16fd4e35f2ee5
chanakya <chanakya.valluri@gmail.com>
2018-10-18 02:45:35 -0700

    imported libraries, started coding class Blockchain

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 18 02:08:11 2018
 
-@author: chanakya
-"""
+# Creating a Blockchain
+
+# To be installed: 
+# Flask
+# Postman HTTP client: https://getpostman.com/
+
+# Importing libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Building a Blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create(proof = 1, prev_hash = '0')
+
+# Mining the Blockchain
+
+
 
