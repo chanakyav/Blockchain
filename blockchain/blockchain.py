8090f53b88beb981416f6e8fdf8b2df60b965490
chanakya <chanakya.valluri@gmail.com>
2018-10-22 16:50:12 -0700

    Finished creating blockchain class

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -19,11 +19,11 @@ class Blockchain:
     
     def __init__(self):
         self.chain = []
-        self.create(proof = 1, prev_hash = '0')
+        self.create_block(proof = 1, prev_hash = '0')
         
     def create_block(self, proof, prev_hash):
         block = {'index': len(self.chain)+1,
-                 'timestamp': str(datetime.date.now()),
+                 'timestamp': str((datetime.datetime.now())),
                  'proof': proof,
                  'prev_hash': prev_hash}
         
@@ -32,7 +32,38 @@ class Blockchain:
     
     def get_prev_block(self):
         return self.chain[-1]
-
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while(check_proof is False):
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexadigest()
+            if(hash_operation[:4] == '0000'):
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block["previous_block "] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block["proof"]
+            proof = block["proof"]
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexadigest()
+            if hash_operation[:4] != "0000":
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+            
 # Mining the Blockchain
 
 
